8cc9a2592e61cc2ea249cadbc60c1652b3e48475
smhg <sam@strictlyphp.com>
2013-11-06 15:25:04 +0100

    comment cleanup

--- test/frequency.js
+++ test/frequency.js
@@ -1,24 +1,5 @@
-var Frequency = require('../lib/frequency');
-
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+var Frequency = require('../lib/frequency'),
+  moment = require('moment');
 
 module.exports = {
   constructor: function (test) {
@@ -108,8 +89,7 @@ module.exports = {
     test.done();
   },
   moment: function (test) {
-    var moment = require('moment'),
-      f = new Frequency(),
+    var f = new Frequency(),
       start = moment(new Date(2013, 8, 2)),
       end = moment(new Date(2013, 8, 9));
 
